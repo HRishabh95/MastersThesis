c709f8d33aecf3f53b9c46e4862b7dffdcc8a662
HRishabh95 <uhrishabh@Gmail.com>
2017-10-02 14:46:49 +0300

    first commit

---
+++ pull.py
@@ -0,0 +1,14 @@
+import git
+#url=git://github.com/olivergierke/repositories-deepdive.git
+def commit_message(url,di, n)
+    repo=git.Repo.clone_from(url,di)
+    heads = repo.heads
+    master = heads.master
+    committs = list(repo.iter_commits('master', max_count=n))
+    f=open("Commit.txt",'w')
+    for i in committs:
+        f.write(i.message.encode("latin1", 'ignore'))
+    f.close()
+
+commit_message("git://github.com/olivergierke/repositories-deepdive.git", "C:/",100)
+
